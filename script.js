c1907dd5177cfcfd2d2eb002cfdd252cb022ddaf
Alaa Adel <alaaadelahmed50@gmail.com>
2023-10-06 19:46:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // var slider1 = new bootstrap.Carousel(document.getElementById('slider1'));
+  var slider4 = new bootstrap.Carousel(document.getElementById("slider4"));
+});
